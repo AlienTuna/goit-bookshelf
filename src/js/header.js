bf20e53f99fcc05bdb73181d82b925e5772133b9
Alexander Neskreba <119747544+alnesk@users.noreply.github.com>
2023-06-17 21:40:36 +0200

    header

--- src/js/header.js
+++ src/js/header.js
@@ -24,17 +24,48 @@
 //     }
 //   }
 
+// (() => {
+//     const refs = {
+//       openMenuBtn: document.querySelector("[data-menu-open]"),
+//       closeMenuBtn: document.querySelector("[data-menu-close]"),
+//       menu: document.querySelector("[data-menu]"),
+//     };
+  
+//     refs.openMenuBtn.addEventListener("click", toggleModal);
+//     refs.closeMenuBtn.addEventListener("click", toggleModal);
+  
+//     function toggleModal() {
+//       refs.menu.classList.toggle("is-open");
+//     }
+//   })();
+
+
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
-      closeMenuBtn: document.querySelector("[data-menu-close]"),
-      menu: document.querySelector("[data-menu]"),
-    };
-  
-    refs.openMenuBtn.addEventListener("click", toggleModal);
-    refs.closeMenuBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.menu.classList.toggle("is-open");
-    }
-  })();
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleModal);
+  refs.closeMenuBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.menu.classList.toggle("is-open");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
