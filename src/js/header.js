e3cf246a079f4efec9849a4a6fc1322a4752fb43
Alexander Neskreba <119747544+alnesk@users.noreply.github.com>
2023-06-20 19:52:11 +0200

    new add

--- src/js/header.js
+++ src/js/header.js
@@ -12,36 +12,3 @@
     refs.menu.classList.toggle("is-open");
   }
 })();
-
-// document.querySelector('.themes').addEventListener('change', (e) =>{
-//   if(e.target.nodeName === 'INPUT'){
-//     document.documentElement.classList.remove('dark', 'light');
-//     document.documentElement.classList.add(e.target.value);
-//   }
-// });
-
- // function to set a given theme/color-scheme
- function setTheme(themeName) {
-  localStorage.setItem('theme', themeName);
-  document.documentElement.className = themeName;
-}
-
-// function to toggle between light and dark theme
-function toggleTheme() {
-  if (localStorage.getItem('theme') === 'theme-dark') {
-      setTheme('theme-light');
-  } else {
-      setTheme('theme-dark');
-  }
-}
-
-// Immediately invoked function to set the theme on initial load
-(function () {
-  if (localStorage.getItem('theme') === 'theme-dark') {
-      setTheme('theme-dark');
-      document.getElementById('slider').checked = false;
-  } else {
-      setTheme('theme-light');
-    document.getElementById('slider').checked = true;
-  }
-})();
