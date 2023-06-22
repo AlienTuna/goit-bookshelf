1a986bd2d5dd2e8e0a4c6c8adce2c8d8d45bb142
AlienTuna <101868546+AlienTuna@users.noreply.github.com>
2023-06-22 11:54:36 +0300

    bugfix

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -9,7 +9,7 @@ var currentPage = 1;
 function generatePagination() {
   var paginationElement = document.getElementById("pagination");
     
-  if (shopList.length === 0) {
+  if (shoppingList.length === 0) {
     paginationElement.style.display = "none";
     return;
   }
