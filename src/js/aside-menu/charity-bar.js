69d31e016f55722eabb3afe9d196c11cd92c63ee
AlienTuna <101868546+AlienTuna@users.noreply.github.com>
2023-06-22 16:17:18 +0300

    bugfix-restore charity-bar

--- src/js/aside-menu/charity-bar.js
+++ src/js/aside-menu/charity-bar.js
@@ -1,41 +1,40 @@
-// import charityList from "/src/js/aside-menu/charity-list";
-// import { tns } from "tiny-slider";
+import charityList from "/src/js/aside-menu/charity-list";
 
-// const charityListEl = document.querySelector('.charity-list');
-// const downBtn = document.querySelector('.charity-slider-btn');
+const charityListEl = document.querySelector('.charity-list');
+const downBtn = document.querySelector('.charity-slider-btn');
 
-// renderCharityBar(charityList);
+renderCharityBar(charityList);
 
-// const slider = tns({
-//   nav: false,
-//   container: '.my-slider',
-//   axis: 'vertical',
-//   speed: 400,
-//   controls: false,
-//   nextButton: '.charity-slider-btn',
-//   controlsContainer: false,
-//   rewind: true,
-//   mouseDrag: true,
-//   responsive: {
-//     375: {
-//       items: 4,
-//     },
-//     768: {
-//       items: 6,
-//     },
-//   }
-// })
-// downBtn.onclick = function () {
-//   slider.goTo('next');
-// };
-// function renderCharityBar(charityList) {
-//   const markup = charityList.map(item => `
-//   <li class="charity-list-item">
-//     <a href="${item.url}">
-//       <img class="charity-fond-img" srcset="${item.img}" alt="${item.title}">
-//     </a>
-//   </li>
-//   `).join('');
-//   charityListEl.insertAdjacentHTML('beforeend',markup);
-//   return;
-// }
+const slider = tns({
+  nav: false,
+  container: '.my-slider',
+  axis: 'vertical',
+  speed: 400,
+  controls: false,
+  nextButton: '.charity-slider-btn',
+  controlsContainer: false,
+  rewind: true,
+  mouseDrag: true,
+  responsive: {
+    375: {
+      items: 4,
+    },
+    768: {
+      items: 6,
+    },
+  }
+})
+downBtn.onclick = function () {
+  slider.goTo('next');
+};
+function renderCharityBar(charityList) {
+  const markup = charityList.map(item => `
+  <li class="charity-list-item">
+    <a href="${item.url}">
+      <img class="charity-fond-img" srcset="${item.img}" alt="${item.title}">
+    </a>
+  </li>
+  `).join('');
+  charityListEl.insertAdjacentHTML('beforeend',markup);
+  return;
+}
